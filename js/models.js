1153996ee7c466f38966cedd4db19ff627dc2978
Magdiel-GVdz <magdiel.gvdz@gmail.com>
2024-01-13 20:58:13 -0600

    fix dup

--- js/models.js
+++ js/models.js
@@ -27,7 +27,7 @@ export default class Models {
     }
 
     getTodos(){
-        return this.todos;
+        return this.todos.map((todo) => ({...todo}));
     }
 
     addTodo(title, description) {
